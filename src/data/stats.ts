f46d917cea018db786de35239a498fd0eaebd99d
guNaoki <gunaoki.ida@gmail.com>
2026-09-08 19:29:25 -0300

    100k!

--- src/data/stats.ts
+++ src/data/stats.ts
@@ -1,5 +1,5 @@
 export const SOCIAL_STATS = {
-  instagramFollowers: "95k",
+  instagramFollowers: "100k",
   tiktokViews: "3.7M - escolher métrica",
   livesRescued: "100 - escolher métrica",
   volunteersCount: "8 - escolher métrica",
